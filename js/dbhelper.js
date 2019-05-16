751f818d3982498f8606fc6343868fc5bc25c176
Wahab Labi <12077170+wlabi@users.noreply.github.com>
2019-05-16 17:25:50 +0100

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -149,7 +149,8 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    //return (`/img/${restaurant.photograph}`);
+    return `https://wlabi.github.io/Restaurant-Reviews-Udacity-FEND/img/${restaurant.photograph}`;
   }
 
   /**
