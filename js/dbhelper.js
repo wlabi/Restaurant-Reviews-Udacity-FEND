23814728d7ea01f5ff6361f834906a3dcae1910b
Wahab Labi <12077170+wlabi@users.noreply.github.com>
2019-05-16 13:46:02 +0100

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -10,7 +10,7 @@ class DBHelper {
   static get DATABASE_URL() {
     //const port = https://wlabi.github.io/Restaurant-Reviews-Udacity-FEND/; // Change this to your server port
     //return `http://localhost:${port}/data/restaurants.json`;
-    //return https://wlabi.github.io/Restaurant-Reviews-Udacity-FEND/;
+    return /data/restaurants.json;
   }
 
   /**
