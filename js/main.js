7d55c7274a808b91786aa78b1809ba1bff5aa6f8
Wahab Labi <12077170+wlabi@users.noreply.github.com>
2019-05-16 13:29:21 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -205,7 +205,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
  */
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('./service-worker.js')
       .then(registration => {
         console.log('Service worker registered');
       })
