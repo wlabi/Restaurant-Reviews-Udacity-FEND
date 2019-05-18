1fb61eb183f91d2a892367a96e6a4cc5a1067db5
Wahab Labi <12077170+wlabi@users.noreply.github.com>
2019-05-18 16:41:33 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(cacheName)
             .then(cache => {
-                return cache.addAll(filesToCache);
+                return cache.addAll(index.html);
             })
     );
 });
