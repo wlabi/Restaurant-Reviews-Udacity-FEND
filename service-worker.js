870b3d7f7f7b4e36d7e22bed279093085a573035
Wahab Labi <12077170+wlabi@users.noreply.github.com>
2019-05-18 17:16:32 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,34 @@
+const cacheName = 'restaurant-reviews-cache-v1';
+const filesToCache = [
+    '/',
+    '/css/styles.css',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/index.html',
+    '/restaurant.html'
+];
+
 // install service worker
 self.addEventListener('install', event => {
-    const cacheName = 'restaurant-reviews-cache-v1';
-
     console.log('Trying to install the service worker and cache static assets');
     event.waitUntil(
         caches.open(cacheName)
             .then(cache => {
-                return cache.addAll(
-                    [
-                    '/',
-                    '/css/styles.css',
-                    '/data/restaurants.json',
-                    '/img/1.jpg',
-                    '/img/2.jpg',
-                    '/img/3.jpg',
-                    '/img/4.jpg',
-                    '/img/5.jpg',
-                    '/img/6.jpg',
-                    '/img/7.jpg',
-                    '/img/8.jpg',
-                    '/img/9.jpg',
-                    '/img/10.jpg',
-                    '/js/dbhelper.js',
-                    '/js/main.js',
-                    '/js/restaurant_info.js',
-                    '/index.html',
-                    '/restaurant.html'
-                    ]
-                );
+                return cache.addAll(filesToCache);
             })
     );
-});
-
-// activate event
-self.addEventListener('activate', event => {
-    console.log('Activating new service worker...');
 });
 
 // fetch event
